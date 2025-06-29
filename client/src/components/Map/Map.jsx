592f12d40aee482b8a7034275c08fbf15785c0a3
itsahmadyaseen <itsahmadyaseen@gmail.com>
2025-06-29 08:07:43 +0530

    UI changes in map

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -4,17 +4,16 @@ import GeoCoderMarker from '../GeoCoderMarker/GeoCoderMarker'
 const Map = ({address, city, country}) => {
   return (
     <MapContainer
-    center={[53.35, 18.8]}
-    zoom={1}
-    scrollWheelZoom={false}
-    style={{
-        height: "40vh",
-        width: "100%",
-        marginTop: "20px",
-        zIndex: 0,
-       
-    }}
-    >
+  center={[53.35, 18.8]} // Default until geocode loads
+  zoom={1}
+  scrollWheelZoom={false}
+  style={{
+    height: "100%",
+    minHeight: "300px",
+    width: "100%",
+    borderRadius: "8px",
+  }}
+>
         <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
         <GeoCoderMarker address={`${address} ${city} ${country}`} />
     </MapContainer>

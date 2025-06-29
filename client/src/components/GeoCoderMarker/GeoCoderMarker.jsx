a91efab49059db40936efb95397eb2dd4de29a05
itsahmadyaseen <itsahmadyaseen@gmail.com>
2025-06-29 07:50:57 +0530

    made map more user friendly

--- client/src/components/GeoCoderMarker/GeoCoderMarker.jsx
+++ client/src/components/GeoCoderMarker/GeoCoderMarker.jsx
@@ -1,38 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { Marker, Popup, useMap } from 'react-leaflet'
-import L from 'leaflet'
-import "leaflet/dist/leaflet.css"
+import React, { useEffect, useState } from "react";
+import { Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import * as ELG from 'esri-leaflet-geocoder'
+import * as ELG from "esri-leaflet-geocoder";
 
-let DefaulIcon = L.icon ({
-    iconUrl : icon, 
-    shadowUrl: iconShadow
-})
-L.Marker.prototype.options.icon = DefaulIcon
+// setup default marker icon
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
+const GeoCoderMarker = ({ address }) => {
+  const map = useMap();
+  const [position, setPosition] = useState(null);
 
-const GeoCoderMarker = ({address}) => {
+  useEffect(() => {
+    if (!address) return;
 
-    const map = useMap()
-    const [position, setPosition] = useState([60, 19])
+    ELG.geocode()
+      .text(address)
+      .run((err, results) => {
+        if (err) {
+          console.error("Geocoding error:", err);
+          return;
+        }
 
-    useEffect(()=> {
-        ELG.geocode().text(address).run((err, results, response)=> {
-            if(results?.results?.length > 0){
-                const {lat, lng} = results?.results[0].latlng
-                setPosition([lat, lng])
-                map.flyTo([lat, lng], 6)
-            }
-        })
-    }, [address])
+        if (results?.results?.length > 0) {
+          const { lat, lng } = results.results[0].latlng;
+          const newPosition = [lat, lng];
+          setPosition(newPosition);
+          map.flyTo(newPosition, 14); // ✅ Zoom in closer
+        } else {
+          console.warn("No geocode results for:", address);
+        }
+      });
+  }, [address, map]);
 
-  return (
-    <Marker position={position} icon={DefaulIcon}>
-        <Popup/>
+  return position ? (
+    <Marker position={position}>
+      <Popup>
+        📍 Location for:
+        <br />
+        <strong>{address}</strong>
+      </Popup>
     </Marker>
-  )
-}
+  ) : null;
+};
 
-export default GeoCoderMarker
+export default GeoCoderMarker;
